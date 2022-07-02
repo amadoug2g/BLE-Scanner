ebdd3e35ff97f12423aa46ac197baeb0faa38188
Amadougaye Cissé <amadou_g@hotmail.fr>
2022-07-02 22:20:58 +0200

    Adding Bluetooth/Location permission requests & activation

--- app/src/main/java/com/playgroundagc/blescanner/MainActivity.kt
+++ app/src/main/java/com/playgroundagc/blescanner/MainActivity.kt
@@ -7,17 +7,22 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.playgroundagc.blescanner.databinding.ActivityMainBinding
 
-private const val ENABLE_BLUETOOTH_CODE = 1
-
 class MainActivity : AppCompatActivity() {
+
+    //region Variables
     private lateinit var binding: ActivityMainBinding
 
     private val bluetoothAdapter: BluetoothAdapter by lazy {
@@ -25,50 +30,167 @@ class MainActivity : AppCompatActivity() {
         bluetoothManager.adapter
     }
 
+    private val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private val isBluetoothPermissionGranted
+        get() = hasPermission(Manifest.permission.BLUETOOTH)
+
+    private val isBluetoothOn
+        get() = bluetoothAdapter.isBluetoothEnabled()
+
+    private val isLocationOn
+        get() = isLocationEnabled()
+    //endregion
+
+    //region Override Methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        checkActivation()
+
         binding.apply {
             lifecycleOwner = this@MainActivity
             executePendingBindings()
             bleScanner.setOnClickListener {
-                if (checkBluetoothStatus()) {
-                    Toast.makeText(applicationContext, "Bluetooth enabled", Toast.LENGTH_SHORT).show()
-                    if (checkBluetoothPermission()) {
-                        Toast.makeText(applicationContext, "Bluetooth granted", Toast.LENGTH_SHORT).show()
-
-                    // TODO: Scan for BLE devices
-
-                    } else {
-                        Toast.makeText(applicationContext, "Bluetooth not granted", Toast.LENGTH_SHORT).show()
-                        askBluetoothPermission()
-                    }
-                } else {
-                    Toast.makeText(applicationContext, "Bluetooth not enabled", Toast.LENGTH_SHORT).show()
-                }
+                checkPermissions()
             }
         }
     }
+    //endregion
 
-    private fun checkBluetoothStatus(): Boolean {
-        return if (bluetoothAdapter.isEnabled) {
-            true
+    //region Permissions
+    private fun checkPermissions() {
+        checkActivation()
+
+        if (isLocationPermissionGranted && isBluetoothPermissionGranted) {
+            if (isLocationOn && isBluetoothOn) {
+                performScan()
+            } else {
+                if (!isLocationOn) activateLocation()
+                if (!isBluetoothOn) activateBluetooth()
+            }
         } else {
-            askBluetoothPermission()
-            false
+            if (!isLocationPermissionGranted) requestLocationPermission()
+            if (!isBluetoothPermissionGranted) requestBluetoothPermission()
         }
     }
 
+    private fun checkActivation(): Boolean {
+        return checkLocationPermission() && checkBluetoothPermission()
+    }
+    //endregion
+
+    //region Bluetooth
     private fun checkBluetoothPermission(): Boolean {
-        return when (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)) {
+        return when (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.BLUETOOTH
+        )) {
             PackageManager.PERMISSION_GRANTED -> true
             else -> false
         }
     }
 
-    private fun askBluetoothPermission() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.BLUETOOTH),
-            ENABLE_BLUETOOTH_CODE)
+    private fun requestBluetoothPermission() {
+        val requestMultiplePermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    Log.d("Permission requests", "${it.key} = ${it.value}")
+                }
+            }
+
+        //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH), BLUETOOTH_REQUEST_CODE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestMultiplePermissions.launch(
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                )
+            )
+        } else {
+            val requestBluetooth =
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                    if (result.resultCode == RESULT_OK) {
+                        "Bluetooth permission granted".toast()
+                    } else {
+                        "Bluetooth permission denied".toast()
+                    }
+                }
+
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            requestBluetooth.launch(enableBtIntent)
+        }
     }
+
+    private fun activateBluetooth() {
+        val startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val intent = result.data
+                    // TODO: Handle the Intent
+                }
+            }
+
+        val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startForResult.launch(intent)
+    }
+    //endregion
+
+    //region Location
+    private fun checkLocationPermission(): Boolean {
+        return when (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )) {
+            PackageManager.PERMISSION_GRANTED -> true
+            else -> false
+        }
+    }
+
+    private fun requestLocationPermission() {
+        val activityResultLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted ->
+                // Handle Permission granted/rejected
+                if (isGranted) {
+                    "Permission granted!".toast()
+                } else {
+                    // Permission is denied
+                    "Permission denied :(".toast()
+                }
+            }
+
+        activityResultLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun activateLocation() {}
+    //endregion
+
+    //region BLE Actions
+    private fun performScan() {}
+    //endregion
+
+    //region Extension Functions
+    private fun String.toast() {
+        Toast.makeText(applicationContext, this, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun Context.isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun BluetoothAdapter.isBluetoothEnabled(): Boolean {
+        return this.isEnabled
+    }
+    //endregion
 }
